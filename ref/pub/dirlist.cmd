0f226573ed074363b67bb8c280fcbd86aa1919dc
indiamcq <india.mcq@gmail.com>
2017-04-01 20:49:58 -0500

    typo fix

--- ref/pub/dirlist.cmd
+++ ref/pub/dirlist.cmd
@@ -2,7 +2,7 @@
 
 :dirlist
 :: Description: Creates a directory list in a file
-:: Depreciated: not in surrent usage
+:: Depreciated: not in current usage
 :: Class: Command - external
 :: Depends on:
 :: dirpath
@@ -12,7 +12,6 @@ set dirpath=%~1
 if not defined dirpath echo missing dirpath input & goto :eof
 set dirlist=%~2
 dir /b "%dirpath%" > "%dirlist%"
-if defined debugdefinefunc echo %endfuncstring% %0 %debugstack%
-goto :eof
+if defined debugdefinefunc echo %endfuncstring% %0 %debugstack%goto :eof
 
 
