326b116631503bb2ddcbdac3443ebae152929104
indiamcq <india.mcq@gmail.com>
2017-04-01 20:58:36 -0500

    updated param order doc and some feed back info

--- ref/pub/encoding.cmd
+++ ref/pub/encoding.cmd
@@ -4,8 +4,8 @@
 :: Description: to check the encoding of a file
 :: Created: 2016-05-17
 :: Required parameters:
-:: activity = validate or check
 :: file
+:: activity = validate or check
 :: Optional parameters:
 :: validateagainst
 :: Depends on:
@@ -19,14 +19,14 @@ set activity=%~2
 set validateagainst=%~3
 call :infile "%testfile%"
 call :nameext "%infile%"
-set command=%encodingchecker% -m %magic% --mime-encoding %infile%
+set command=%encodingchecker% -m %magic% --mime-encoding "%infile%"
 FOR /F "tokens=1-2" %%A IN ('%command%') DO set fencoding=%%B
-rem echo encoding check = %fencoding%
+
 if "%activity%" == "validate" (
     if "%fencoding%" == "%validateagainst%"  (
-        rem %validateagainst%
-      ) else "%fencoding%" == "us-ascii" (
-        rem us-ascii
+        echo Encoding is %fencoding% for file %nameext%.
+      ) else if "%fencoding%" == "us-ascii" (
+        echo Encoding is %fencoding% not %validateagainst% but is usable.
       ) else (
       echo File %nameext% encoding is invalid! 
       echo Encoding found to be %fencoding%! But it was expected to be %validateagainst%.
