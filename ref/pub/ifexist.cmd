74ab4d36cb185c7e430ef1ec3fb2a47c14cb666c
indiamcq <india.mcq@gmail.com>
2017-04-01 21:02:41 -0500

    the added func and addtext option was causing crash when file with spaces in it was used. those options no longer available.

--- ref/pub/ifexist.cmd
+++ ref/pub/ifexist.cmd
@@ -27,7 +27,9 @@ set param6=%~6
 if not defined testfile echo missing testfile parameter & goto :eof
 if not defined action echo missing action parameter & goto :eof
 rem if defined param4 set param4=%param4:'="%
+
 call :nameext "%~1"
+
 if exist "%testfile%" (
   rem say what will happen
   if "%action%" == "xcopy" echo %action% %param4% "%testfile%" "%param3%"
@@ -35,14 +37,14 @@ if exist "%testfile%" (
   if "%action%" == "move" echo %action% %param4% "%testfile%" "%param3%"
   if "%action%" == "rename" echo %action% "%testfile%" "%param3%"
   if "%action%" == "del" echo %action% %param4% "%testfile%"
-  if "%action%" == "func" echo call :%param3% "%param4%"
+  rem if "%action%" == "func" echo call :%param3% "%param4%"
   if "%action%" == "command" echo call :command "%param3%" "%param4%"  "%param5%"
   if "%action%" == "command2file" echo call :command2file "%param3%" "%param4%" "%param5%" "%param6%"
   if "%action%" == "tasklist" echo call :tasklist "%param3%" "%param4%"
   if "%action%" == "append" echo copy "%param3%"+"%testfile%" "%param3%"
   if "%action%" == "appendtext" echo copy /A "%param3%"+"%testfile%" "%param3%"
   if "%action%" == "appendbin" echo copy /b "%param3%"+"%testfile%" "%param3%"
-  if "%action%" == "addtext" echo  echo %param3% ^>^> "%param4%"
+  rem if "%action%" == "addtext" echo  echo %param3% ^>^> "%param4%"
   if "%action%" == "type" echo type "%testfile%" ^>^> "%param3%"
   if "%action%" == "emptyfile" echo echo. ^> "%testfile%"
   rem now do what was said
@@ -51,14 +53,14 @@ if exist "%testfile%" (
   if "%action%" == "move" %action% %param4% "%testfile%" "%param3%"
   if "%action%" == "rename" %action% "%testfile%" "%param3%"
   if "%action%" == "del" %action% /Q "%testfile%"
-  if "%action%" == "func" call :%param3% "%param4%"
+  rem if "%action%" == "func" call :%param3% "%param4%"
   if "%action%" == "command" call :command "%param3%" "%param4%"  "%param5%"
   if "%action%" == "command2file" call :command2file "%param3%" "%param4%" "%param5%" "%param6%"
   if "%action%" == "tasklist" call :tasklist "%param3%" "%param4%"
   if "%action%" == "append" copy "%param3%"+"%testfile%" "%param3%"
   if "%action%" == "appendtext" copy /A "%param3%"+"%testfile%" /A "%param3%" /A
   if "%action%" == "append" copy /b "%param3%"+"%testfile%" /b "%param3%" /b
-  if "%action%" == "addtext" echo %param3% >> "%param4%"
+  rem if "%action%" == "addtext" echo %param3% >> "%param4%"
   if "%action%" == "type" type "%testfile%" >> "%param3%"
   if "%action%" == "emptyfile" echo.  > "%testfile%"
   if "%action%" == "fatal" (
@@ -70,7 +72,7 @@ if exist "%testfile%" (
     goto :eof
   )
 ) else (
-  echo %testfile% was not found to %action%
+  echo "%testfile%" was not found to %action%
 )
 if defined masterdebug call :funcdebug %0 endgoto :eof
 
