358e252d05a027484e21de814a1d29b387a2601f
indiamcq <india.mcq@gmail.com>
2017-04-01 21:32:04 -0500

    new xslt only func

---
+++ ref/pub/xvar.cmd
@@ -0,0 +1,7 @@
+
+
+:xvar
+:: Description: This is an XSLT only instruction to process a param, DOS variables not allowed in set.
+:: Note: not used by this batch command. The xvar only creates an XSLT variable but not a DOS variable. Can be used when there are long variables intended for XSLT only.goto :eof
+
+
